cedfa0dfc651cfe3fd920e5e24f41be85d1711af
Yaning Hu <helenahu@users.noreply.github.com>
2020-08-20 10:29:31 -0500

    fixed box-shadow bug for Carpool image

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -3,7 +3,7 @@ import "../styles/portfolio.css";
 import HedwigBuyerPic from "../images/Hedwig-buyer-pic.svg"
 import HedwigVendorPic from "../images/Hedwig-vendor-pic.svg"
 import CarpoolPic from "../images/Carpool-pic.svg"
-
+import { Link } from "gatsby";
 export default function Portfolio() {
   return (
     <div className="PortfolioDiv">
@@ -24,8 +24,12 @@ export default function Portfolio() {
                 <button>View</button>
             </div>
             <div id="hedwig-pics" className="project-img-right" >
-                <img src={HedwigBuyerPic} className="project-img" id="buyer" width="45%"/>
-                <img src={HedwigVendorPic} className="project-img" id="vendor" width="45%"/>
+                <Link className="img-links" className="hedwig-links" to="/my-works/">
+                    <img src={HedwigBuyerPic} className="project-img" id="buyer" width="45%"/>
+                </Link>
+                <Link className="img-links" className="hedwig-links" to="/my-works/">
+                    <img src={HedwigVendorPic} className="project-img" id="vendor" width="45%"/>
+                </Link>
             </div>
             
 
@@ -34,7 +38,11 @@ export default function Portfolio() {
         </div>
         {/* Carpool Project */}
         <div className="project">
-            <img src={CarpoolPic} className="project-img-left" className="project-img" width="45%"/>
+            <div id="carpool-pics" className="project-img-left" >
+                <Link className="img-links" className="hedwig-links" to="/my-works/">
+                    <img src={CarpoolPic} className="project-img" width="90%"/>
+                </Link>
+            </div>
                 <div className="project-text">
                     <h1 className="project-title">Carpool</h1>
                     <p className="project-description">A carpooling app that makes trips much easer for Rice Students.</p>
